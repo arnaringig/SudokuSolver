93efc60ef6a49f1c6641920f7423c65037df78df
arnaringig <arnaringigunnarsson@icloud.com>
2019-11-11 16:53:28 +0000

    comments

--- Sudoku.py
+++ Sudoku.py
@@ -41,28 +41,28 @@ class Sudoku:
 		return temp
 
 	def solve(self):
-		self.recursion(0,0,1)
+		self.recursion(0,0,1)           # We start the recursion at the top right corner of the sudoku problem and try the inital value as 1
 		if self.problem == self.problemSolution:
 			print( "solved" )
 		for i in self.problem:
 			print(i)
 
 	def recursion(self,c,r,v):
-		if r == 9: return
-		if self.isFixed(c, r):
-			c,r = self.moveToNext(c, r)
-		while self.breaksRules(c, r, v) and v in range(10):
-			v += 1
-		if v == 10:
-			self.problem[r][c] = 0
-			c,r = self.moveBack(c, r)
-			v = self.problem[r][c]	
-			self.recursion(c, r, v+1)
+		if r == 9: return				# If we have reached a place where the row is number 9, we have found all the values and solved the problem, so we return.
+		if self.isFixed(c, r):          # check if the first square is a fixed number or empty.
+			c,r = self.moveToNext(c, r) # If fixed we move one right (or until there´s an empty square)
+		while self.breaksRules(c, r, v) and v in range(10):  
+			v += 1                      # Then we try numbers from 1 to 9 and see if there´s a number that doesn´t break the rules.
+		if v == 10:                     # If v gets to 10 it means no number didn´t break the rules so we have to backtrack.
+			self.problem[r][c] = 0	    # When backtracking we must "empty" the current square so we can check if it´s empty when moving forward again.
+			c,r = self.moveBack(c, r)   # Moves us to the next point that isn´t fixed.
+			v = self.problem[r][c]	    # Let´s get the value from the square we´re backtracking to so we can increase it by 1
+			self.recursion(c, r, v+1)   # We backtracked to this point and now we go forward again by increasing this square´s value by 1
 		else:
-			self.problem[r][c] = v
-			c,r = self.moveToNext(c, r)			
-			v = 1
-			self.recursion(c, r, v) 
+			self.problem[r][c] = v      # So we found a value between 1 and 9 and it doesn´t break the rules. We put that value in the square
+			c,r = self.moveToNext(c, r)	# Then we find the next "legal" square, one that isn´t fixed	
+			v = 1						# and we set v to 1 so 1 will be the first value we try in the next iteration
+			self.recursion(c, r, v) 	# So we have set c, r and v to the appropriate values and perform a recursive call to the method.
 
 	# takes in current position
 	# skips fixed ones
